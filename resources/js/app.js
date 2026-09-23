b2755e2750dfc4b64e43976457e16d5576806cf0
amberlex78 <amberlex78@gmail.com>
2026-09-23 19:27:10 +0300

    fix(charts): pin ApexCharts and scope chart import locally

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,12 @@
 import { createPopper } from '@popperjs/core';
 import './bootstrap';
 import Alpine from 'alpinejs';
-import ApexCharts from 'apexcharts';
 
 // flatpickr
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 window.Alpine = Alpine;
 window.createPopper = createPopper;
-window.ApexCharts = ApexCharts;
 window.flatpickr = flatpickr;
 
 Alpine.start();
